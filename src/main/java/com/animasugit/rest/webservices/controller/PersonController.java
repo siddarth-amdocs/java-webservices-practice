3ac2693292e38d214c6f153db2bc55f1a83be7f1
Siddarth Sharma <siddarthsharma1077@gmail.com>
2021-08-03 15:58:17 +0530

    dynamic versioning

--- src/main/java/com/animasugit/rest/webservices/controller/PersonController.java
+++ src/main/java/com/animasugit/rest/webservices/controller/PersonController.java
@@ -10,13 +10,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
     
-    @GetMapping("v1/person")
+    // URI PARAMETER
+    @GetMapping("person/v1")
     public PersonV1 personV1(){
         return new PersonV1("Bob Charlie");
     }
-
-    @GetMapping("v2/person")
+    @GetMapping("person/v2")
     public PersonV2 personV2(){
         return new PersonV2(new Name("Bob","Charlie"));
     }
+
+    // REQUEST PARAMETER
+    @GetMapping(value = "person/param",params="version=1")
+    public PersonV1 paramV1(){
+        return new PersonV1("Bob Charlie");
+    }
+    @GetMapping(value = "person/param",params="version=2")
+    public PersonV2 paramV2(){
+        return new PersonV2(new Name("Bob","Charlie"));
+    }
+
+    // NORMAL HEADER
+    @GetMapping(value = "person/header",headers="X_API_VERSION=1")
+    public PersonV1 headerV1(){
+        return new PersonV1("Bob Charlie");
+    }
+    @GetMapping(value = "person/param",headers="X_API_VERSION=2")
+    public PersonV2 headerV2(){
+        return new PersonV2(new Name("Bob","Charlie"));
+    }
+
+    // ACCEPT HEADER
+    @GetMapping(value = "person/produces",produces = "application/vnd.company.app-v1+json")
+    public PersonV1 producesV1(){
+        return new PersonV1("Bob Charlie");
+    }
+    @GetMapping(value = "person/produces",produces = "application/vnd.company.app-v2+json")
+    public PersonV2 producesV2(){
+        return new PersonV2(new Name("Bob","Charlie"));
+    }
+
+
 }
