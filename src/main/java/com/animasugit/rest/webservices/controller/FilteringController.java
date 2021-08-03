b64400b46310283fe1bc9b18a5897e3b7608c151
Siddarth Sharma <siddarthsharma1077@gmail.com>
2021-08-03 15:29:05 +0530

    static filtering

---
+++ src/main/java/com/animasugit/rest/webservices/controller/FilteringController.java
@@ -0,0 +1,13 @@
+package com.animasugit.rest.webservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+    
+    @GetMapping("/filtering")
+    public SomeBean retrieveSomeBean(){
+        return new SomeBean("value1","value2","value3");
+    }
+}
---
+++ src/main/java/com/animasugit/rest/webservices/controller/SomeBean.java
@@ -0,0 +1,44 @@
+package com.animasugit.rest.webservices.controller;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class SomeBean {
+    private String field1;
+    private String field2;
+    @JsonIgnore
+    private String field3;
+
+    public SomeBean() {
+    }
+
+    public SomeBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return this.field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String getField2() {
+        return this.field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public String getField3() {
+        return this.field3;
+    }
+
+    public void setField3(String field3) {
+        this.field3 = field3;
+    }
+
+}
