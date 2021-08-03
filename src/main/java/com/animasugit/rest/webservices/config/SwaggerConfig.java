10c8fcd00f965f9944d215e2430a6ef2ff36d416
Siddarth Sharma <siddarthsharma1077@gmail.com>
2021-08-03 15:08:26 +0530

    actuator setup

--- src/main/java/com/animasugit/rest/webservices/config/SwaggerConfig.java
+++ src/main/java/com/animasugit/rest/webservices/config/SwaggerConfig.java
@@ -13,8 +13,7 @@ import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
 
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,19 +22,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-    private static final Contact DEFAULT_CONTACT = new Contact("Siddarth Sharma","https://animasugit.com","siddarthsharma1077@gmail.com");
-    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API", "Awesome API description", "1.0.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+    //private static final Contact DEFAULT_CONTACT = new Contact("Siddarth Sharma","https://animasugit.com","siddarthsharma1077@gmail.com");
+  //  private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API", "Awesome API description", "1.0.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
     private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json","application/xml"));
 
     @Bean
     public Docket api() {
-       return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO).produces(DEFAULT_PRODUCES_AND_CONSUMES).consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+       return new Docket(DocumentationType.SWAGGER_2)
+       //.apiInfo(DEFAULT_API_INFO)
+       .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+       .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 
     @Bean
     public LinkDiscoverers discoverers() {
         List<LinkDiscoverer> plugins = new ArrayList<>();
         plugins.add(new CollectionJsonLinkDiscoverer());
-        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+        return new LinkDiscoverers(SimplePluginRegistry.of(plugins));
     }
 }
