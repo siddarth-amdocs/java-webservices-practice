d1f0a021bf307dedc0ffde5e6c99d46f04b00204
Siddarth Sharma <siddarthsharma1077@gmail.com>
2021-08-03 12:31:03 +0530

    Internationalization

--- src/main/java/com/animasugit/rest/webservices/controller/HelloWorldController.java
+++ src/main/java/com/animasugit/rest/webservices/controller/HelloWorldController.java
@@ -1,15 +1,18 @@
 package com.animasugit.rest.webservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 //Controller
 @RestController
 public class HelloWorldController {
-    //GET
-    //URI - /hello-world
-    //method - "Hello World"
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
-        return "Hello World";
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
